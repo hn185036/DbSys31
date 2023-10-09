5ee3199882063f302b9bdc266fd478856d7e85ba
hn185036 <hn185036@ncr.com>
2023-10-09 18:13:55 +0800

    Update db.sql

--- db.sql
+++ db.sql
@@ -21,3 +21,14 @@ userInfEmail varchar not null,
 userId integer,
 FOREIGN KEY (userId) REFERENCES UserAccount(userId)
 );
+
+CREATE TABLE [dbo].[Role](
+	[roleId] [int] IDENTITY(1,1) NOT NULL,
+	[roleName] [nvarchar](50) NULL,
+	[roleDescription] [nvarchar](100) NULL,
+ CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED 
+(
+	[roleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
